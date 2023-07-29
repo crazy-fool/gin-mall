c5c4be82eb8af594f0293c6915d442ae407da88a
crazy-fool <1223024890@qq.com>
2023-07-29 10:46:37 +0800

    基础搭建

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.20
 
 require (
 	github.com/gin-gonic/gin v1.9.1
+	github.com/golang-jwt/jwt/v5
 	github.com/google/uuid v1.3.0
+	github.com/google/wire v0.5.0
 	github.com/pkg/errors v0.9.1
 	github.com/sony/sonyflake v1.1.0
 	github.com/spf13/viper v1.16.0
---
+++ internal/handler/handler.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"gin-mall/internal/middleware"
+	"gin-mall/pkg/log"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	logger *log.Logger
+}
+
+func NewHandler(logger *log.Logger) *Handler {
+	return &Handler{
+		logger: logger,
+	}
+}
+func GetUserIdFromCtx(ctx *gin.Context) string {
+	v, exists := ctx.Get("claims")
+	if !exists {
+		return ""
+	}
+	return v.(*middleware.MyCustomClaims).UserId
+}
