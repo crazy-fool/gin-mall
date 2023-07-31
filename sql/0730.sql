e8600f63327a991d79fd87e105fc7974ee51b8db
wang.zhenhua <wang.zh@sand.com.cn>
2023-07-31 10:04:33 +0800

    m

--- sql/0730.sql
+++ sql/0730.sql
@@ -2,11 +2,11 @@
 --  用户表
 CREATE TABLE `hmh_customer` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `code` varchar(50) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'code',
-    `name` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '姓名',
-    `account` varchar(20) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '账号',
-    `password` varchar(64) COLLATE utf8mb4_general_ci DEFAULT '' COMMENT '密码',
-    `mobile` varchar(15) COLLATE utf8mb4_general_ci DEFAULT '' COMMENT '手机号',
+    `code` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'code',
+    `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '姓名',
+    `account` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '账号',
+    `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '' COMMENT '密码',
+    `mobile` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '' COMMENT '手机号',
     `last_login_at` datetime DEFAULT NULL COMMENT '最近登录时间',
     `created_at` datetime DEFAULT NULL,
     `updated_at` datetime DEFAULT NULL,
@@ -15,4 +15,4 @@ CREATE TABLE `hmh_customer` (
     UNIQUE KEY `un_account` (`account`),
     UNIQUE KEY `un_code` (`code`),
     KEY `idx_deleted_at` (`deleted_at`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
