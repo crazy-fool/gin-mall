a3643e7b96231822a35718bb9afb3cbccf47dc58
wang.zhenhua <wang.zh@sand.com.cn>
2023-08-14 18:06:14 +0800

    s

--- cmd/pratice/main.go
+++ cmd/pratice/main.go
@@ -2,21 +2,16 @@ package main
 
 import (
 	"fmt"
-	"gin-mall/pkg/helper/aesutil"
+	"gin-mall/pkg/helper/sign"
 )
 
 func main() {
-	key := []byte("12345678901234567890123456789012")
-	aes := aesutil.NewPkcs7Cbc(key)
-	origin := "123456789"
-	encrypted, err := aes.Base64Encrypted(origin)
-	fmt.Println("加密：", encrypted)
-	//encrypted = "tVIs1T89WnKMXFMUdO/BUA=="
-	decrypted, err := aes.Base64Decrypted(encrypted)
-	if err != nil {
-		fmt.Println(err)
-		return
+	sign := sign.NewRsaSign("", "")
+	params := map[string]any{
+		"a": 123,
+		"b": "ccc",
+		"s": "12.3",
 	}
-	fmt.Println("ccccccc", err, decrypted)
-
+	str := sign.SortParams(params)
+	fmt.Println(str)
 }
